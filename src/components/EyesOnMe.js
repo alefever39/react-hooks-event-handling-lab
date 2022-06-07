a6e5c192c63ee458ddbb5aeae24fe92bdba5c536
Allie LeFever <alefever39@gmail.com>
2022-06-07 11:17:11 -0600

    commit tree 86cd3eeb88971c3010f65c08cef67312c00837ab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React, { useState } from 'react';
+
+const EyesOnMe = () => {
+
+    function handleOnFocus() {
+        console.log('Good!')
+    }
+
+    function handleOnBlur() {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return(
+        <button onFocus={handleOnFocus} onBlur={handleOnBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
-// Code Keypad Component Here
+import React, { useState } from 'react';
+
+const Keypad = () => {
+
+    function handleOnChange() {
+        console.log('Entering password...')
+    }
+
+    return(
+        <input type="password" onChange={handleOnChange}/>
+    )
+}
+
+export default Keypad
